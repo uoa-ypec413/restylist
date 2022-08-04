818126b257a1689dd34e562b825c0e98e36ff3ce
uoa-ypec413 <ypec413@aucklanduni.ac.nz>
2022-08-04 15:04:49 +1200

    :heavy_plus_sign: Add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      "cotton-candy": "#FFCBE6",
+      "cherry-blossom": "#FFEEF7",
+      "baby-blue": "#CEE5FF",
+    },
+  },
+  plugins: [],
+};
